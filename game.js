16dbd0688986dd2c051a4464f35071372736f415
טל בנו <talbeno123@gmail.com>
2022-01-15 03:28:00 +0200

    end of day 1, finished drawing the board, task for tomorrow: start writing the game itself

--- game.js
+++ game.js
@@ -1,15 +1,26 @@
-function createBoared(height, width) {
+function createBoard(height, width) {
     let board = new Array(width);
     for (let i = 0; i < board.length; i++) {
         board[i] = new Array(height);
+        for (let j = 0; j < board[i].length; j++) {
+            board[i][j] = new Cell();
+        }
     }
+
+    return board;
 }
+
+let gameState = "BEFORE"; //options are: "BEFORE", "AFTER", "PLAYING"
+let faceState = "SMILE"; //options are: "SMILE", "OOH", "WIN", "DEAD", "PRESSED"
+let time = 0;
+let totalFlagged = 0;
 
 class Cell {
     constructor() {
         this.revealed = false;
-        this.value = "0";
+        this.value = 0;
         this.mine = false;
         this.marked = false;
+        this.tapped = false;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let gameDetail = Expert;
+let board = createBoard(gameDetail.height, gameDetail.width);
 let canvas;
 let dbx = 20;
 let dby = 104;
@@ -29,7 +30,8 @@ let pictures = {
         dead: "",
         ooh: "",
         smile: "",
-        win: ""
+        win: "",
+        pressed: ""
     },
     time: []
 };
@@ -38,6 +40,7 @@ function preload() {
     for (let i = 0; i <= 8; i++) {
         pictures.board.numbers[i] = loadImage(`https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/board/numbers/open${i}.png`);
     }
+
     pictures.board.covered = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/board/covered.png");
     pictures.board.flagged = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/board/flagged.png");
     pictures.board.bombs.wrong = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/board/wrong%20mark.png");
@@ -58,9 +61,10 @@ function preload() {
     pictures.faces.ooh = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/faces/ooh%20face.png");
     pictures.faces.smile = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/faces/smile%20face.png");
     pictures.faces.win = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/faces/win%20face.png");
+    pictures.faces.pressed = loadImage("https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/faces/pressed%20face.png");
 
     for (let i = 0; i <= 9; i++) {
-        pictures.time = loadImage(`https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/time%20numbers/time${i}.png`);
+        pictures.time[i] = loadImage(`https://raw.githubusercontent.com/TalBeno314/temp-test/master/minesweeper%20data/time%20numbers/time${i}.png`);
     }
 }
 
@@ -69,14 +73,92 @@ function setup() {
 }
 
 function draw() {
-    background(220);
+    background("#bcbdbc");
+    //drawing the board
     for (let i = 0; i < gameDetail.width; i++) {
         for (let j = 0; j < gameDetail.height; j++) {
-            image()
+            let cell = board[i][j];
+            if (cell.revealed) {
+                if (cell.bomb) {
+                    if (cell.marked) {
+                        image(pictures.board.bombs.wrong, i * 32 + dbx, j * 32 + dby);
+                    } else if (cell.tapped) {
+                        image(pictures.board.bombs.tapped, i * 32 + dbx, j * 32 + dby);
+                    } else {
+                        image(pictures.board.bombs.regular, i * 32 + dbx, j * 32 + dby);
+                    }
+                } else {
+                    image(pictures.board.numbers[cell.value]);
+                }
+            } else {
+                if (cell.flagged) {
+                    image(pictures.board.flagged, i * 32 + dbx, j * 32 + dby);
+                } else {
+                    image(pictures.board.covered, i * 32 + dbx, j * 32 + dby);
+                }
+            }
         }
     }
+
+    //drawing the border
+    image(pictures.border.top_left, 0, 0);
+    image(pictures.border.top_right, width - 20, 0);
+    image(pictures.border.tb, 20, 0, width - 40, 20);
+    image(pictures.border.bottom_left, 0, height - 20);
+    image(pictures.border.bottom_right, width - 20, height - 20);
+    image(pictures.border.tb, 20, height - 20, width - 40, 20);
+
+    image(pictures.border.tb, 20, 84, width - 40, 20);
+    image(pictures.border.joint_left, 0, 84);
+    image(pictures.border.joint_right, width - 20, 84);
+
+    image(pictures.border.side_long, 0, 20);
+    image(pictures.border.side_long, width - 20, 20);
+    image(pictures.border.side_short, 0, 104, 20, height - 124);
+    image(pictures.border.side_short, width - 20, 104, 20, height - 124);
+
+    //drawing the smily face
+    if (faceState == "SMILE") {
+        image(pictures.faces.smile, (width - 52) / 2, 26);
+    } else if (faceState == "OOH") {
+        image(pictures.faces.ooh, (width - 52) / 2, 26);
+    } else if (faceState == "WIN") {
+        image(pictures.faces.win, (width - 52) / 2, 26);
+    } else if (faceState == "LOSE") {
+        image(pictures.faces.dead, (width - 52) / 2, 26);
+    } else if (faceState == "PRESSED") {
+        image(pictures.faces.pressed, (width - 52) / 2, 26);
+    }
+
+    //drawing the timer
+    let tempTime = convertToThree(time);
+
+    image(pictures.time[tempTime[0]], width - 56, 31);
+    image(pictures.time[tempTime[1]], width - 82, 31);
+    image(pictures.time[tempTime[2]], width - 108, 31);
+
+    //drawing the mine count
+    let tempMine = convertToThree((gameDetail.mines - totalFlagged));
+
+    image(pictures.time[tempMine[2]], 30, 31);
+    image(pictures.time[tempMine[1]], 56, 31);
+    image(pictures.time[tempMine[0]], 82, 31);
 }
 
 function Canvas() {
-    canvas = createCanvas(gameDetail.width * 32 + 40, gameDetail.height * 32 + 40);
+    console.log(gameDetail);
+    canvas = createCanvas(gameDetail.width * 32 + 40, gameDetail.height * 32 + 124);
+}
+
+function convertToThree(num) {
+    let newNum = [];
+    if (num < 1000) {
+        newNum[0] = num % 10;
+        newNum[1] = Math.floor((num % 100) / 10)
+        newNum[2] = Math.floor((num % 1000) / 100);
+    } else {
+        newNum = ["9", "9", "9"]
+    }
+
+    return newNum;
 }
